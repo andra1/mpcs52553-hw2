5391aaee47a5b4d7743026e8d822002d62e6ac08
andra1 <kaushik.andra92@gmail.com>
2018-01-19 16:48:31 -0600

    finished two challenges

--- bike_count.rb
+++ bike_count.rb
@@ -1,1 +1,20 @@
 # Your code goes here
+require 'json'
+require 'net/http'
+
+url = 'https://feeds.divvybikes.com/stations/stations.json'
+#read json object from url into Ruby HASH
+uri = URI(url)
+response = Net::HTTP.get(uri)
+data_hash = JSON.parse(response)
+stations = data_hash["stationBeanList"]
+count = 0
+
+stations.each do |station|
+	value = station["totalDocks"]
+	count = count + value
+end
+
+puts count
+
+
--- location.rb
+++ location.rb
@@ -1,1 +1,31 @@
 # Your code goes here
+require 'json'
+require 'net/http'
+
+#First read in address from address.txt file
+
+def get_json(url)
+	uri = URI(url)
+	response = Net::HTTP.get(uri)
+	data_hash = JSON.parse(response)
+	#puts data_hash
+	return data_hash
+end
+
+address = File.open("address.txt").first
+
+api_key = '&key=AIzaSyCK_MBgKTXZjpWvLvb5WpxmNk2mafDNcfo'
+base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+request = base_url + address + api_key
+
+data = get_json(request)
+
+lat = 0
+long = 0
+data["results"].each do |field|
+	lat = field["geometry"]["location"]["lat"]
+	long = field["geometry"]["location"]["lng"]
+end
+
+puts lat
+puts long
